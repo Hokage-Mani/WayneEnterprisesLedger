fdf622b47ac9bdb05a6af2b0bbc13bbaef3616cd
Hokage-Mani <armaniburton.nyc@gmail.com>
2026-04-26 05:56:40 -0400

    My intiial commit while also making some headway. I created my classes I plan on putting to work when completing my ledger, I was going to have two seperate classes for writing to the file and reading from the file but I wanted something neat and not "too much" as possible so I made one class for it FileManager.

---
+++ src/main/java/org/example/Transaction.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Transaction {
+
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private double amount;
+    private boolean isDeposited;
+}
